de41edb6f1547f19ff4e34a0bc409a8417327078
Ilham-Nur <ilonoer26@gmail.com>
2025-08-20 09:47:54 +0700

    push origin ilham

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         if (config('app.env') === 'production' || app()->environment('local')) {
-            URL::forceScheme('https');
-        }
+        //  if (config('app.env') === 'production' || app()->environment('local')) {
+        //     URL::forceScheme('https');
+        // }
     }
 }
